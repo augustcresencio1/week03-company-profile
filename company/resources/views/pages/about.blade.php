915e3cfae8bac88ed334d6c468a374c80b9b6f9c
August Cresencio <augustcresencio022@gmail.com>
2026-08-14 16:18:42 +0800

    lezgo

--- company/resources/views/pages/about.blade.php
+++ company/resources/views/pages/about.blade.php
@@ -804,7 +804,7 @@
             <p>
                 We explore fresh ideas, modern designs,
                 and new ways to make everyday footwear
-                better let's go baby i lo .
+                better let's go baby i love  .
             </p>
 
         </div>
